db009bef82a3b0ba4c18c5b324c8c6072df6594e
dicky <dickykusmawan@gmail.com>
2019-09-26 19:25:45 +0700

    ubah

--- tugas1.php
+++ tugas1.php
@@ -2,21 +2,23 @@
   
 
 <div class="media">
-  <img src="assets/20170801358.jpg" class="mr-3">
+  <img src="https://scontent-sin6-2.xx.fbcdn.net/v/t1.0-1/p160x160/30716460_1881961045189497_1627614701434496514_n.jpg?_nc_cat=107&_nc_oc=AQmlW-gTuwLBAj8Fb0FtsjxCS0HlO6-Q01ZKpGWewS2nWMJ2PWIvpdocOQA4D4COhcQ&_nc_ht=scontent-sin6-2.xx&oh=267bb1ccfd18f3aa48fe39dbb78f7a34&oe=5DF8993A" class="mr-3">
   <div class="media-body">
     <h5 class="mt-0">Cyntia Wulandari</h5>
     Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
   </div>
 </div>
 <div class="media">
-  <img src="assets/20170801358.jpg" class="mr-3">
+  <img src="https://scontent-sin6-2.xx.fbcdn.net/v/t1.0-1/c1.0.160.160a/p160x160/17098395_1532148686843826_2495492239166685187_n.jpg?_nc_cat=107&_nc_oc=AQkzNHlVwkdM9IMAGGvjtU8_S3u7e1auiJy2TShyacrnlma0YyijB1Ebe3RbX7NaWi4&_nc_ht=scontent-sin6-2.xx&oh=9a19b043b81ce7b83b616fca2c226052&oe=5DFE729B" class="mr-3">
   <div class="media-body">
     <h5 class="mt-0">Dicky Kusmawan</h5>
-    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+    Nim : 20170801374
+    <br>Hobi : Berenang, Memanjat Tembok, Rebahan
+    <br>Social Media : <a href="https://www.instagram.com/kusmawnd/">instagram</a>
   </div>
 </div>
 <div class="media">
-  <img src="assets/20170801358.jpg" class="mr-3">
+  <img src="https://scontent-sin6-2.xx.fbcdn.net/v/t1.0-1/p160x160/22688683_1466390526729388_7457300279444274291_n.jpg?_nc_cat=109&_nc_oc=AQkeJYEjmVuDZE7a8DrGQfGq8y9MpyJFsx1updkWUUQRirMMrGfBt6peIo5t_ki1quA&_nc_ht=scontent-sin6-2.xx&oh=908b746ba9c182cdc14ef1d8e7c72901&oe=5E3988D7" class="mr-3">
   <div class="media-body">
     <h5 class="mt-0">Muh Sukma Saputra</h5>
     Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
